825738ebdb52354c8f7c54c5abcf3ef873b280a3
Colin McKeon <14mckeon@gmail.com>
2017-01-18 11:58:22 -0500

    sadf

--- app/scripts/components/colinmck.jsx
+++ app/scripts/components/colinmck.jsx
@@ -6,7 +6,7 @@ var NavBar = React.createClass({
     return(
       <div>
 
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded navbar-inverse bg-inverse">
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -14,20 +14,13 @@ var NavBar = React.createClass({
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
             </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
           </div>
         </nav>
 
